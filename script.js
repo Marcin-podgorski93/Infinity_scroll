cf441c6f318c0e46f5822ac2b6ab9f7c3208e189
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-04-12 13:38:25 +0200

    create function forEach and fetch API from Unsplash and add them to clss image-container

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const count = 10;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=AdPm_uRRDdyvWo1saPRJHxalgYitsIg-hHjlZDsdRaY&count=${count}`;
+const container = document.getElementById('image-container');
 
+const count = 2;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=LC1Jz5zQmU9Vcle2MJkb6BVzvZAkC1TiZeHRU5PBhR4&count=${count}`;
+
+let photoArray = [];
+
+// Create Elements for Links $ Photos, Add to DOM 
+function showPictures() {
+    // Run function for each object in photoArray
+    photoArray.forEach(photo => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        // Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        // Put <img> inside <a>, then put both inside image-container Element
+        // <img> to <a>
+        item.appendChild(img);
+        // add <a> to image-container
+        container.appendChild(item);
+    });
+}
+
+
+// Get photos from Unsplash API 
 async function fetchImage() {
-    const repo = await fetch(apiUrl);
-    const data = await repo.json();
-    console.log(data);
+    try {
+        const repo = await fetch(apiUrl);
+        photoArray = await repo.json();
+        showPictures();
+    } catch (error) {
+        // Catch Error Here
+    }
 }
 
 fetchImage()
